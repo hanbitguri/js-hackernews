5f9c090c734863502bcb583bcd0c6147a963e732
전한빛 <asjask123@gmail.com>
2022-12-11 23:38:26 +0900

    use event : hashcchange to implement page route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ajax = new XMLHttpRequest();
 const newsURL = 'https://api.hnpwa.com/v0/news/1.json'
 const contentURL = `https://api.hnpwa.com/v0/item/@id.json`
+const content = document.createElement('div')
 ajax.open('GET',newsURL,false);
 ajax.send();
 
@@ -9,10 +10,23 @@ const newsFeed = JSON.parse(ajax.response)
 const root = document.querySelector('.root')
 const ul = document.createElement('ul')
 
+window.addEventListener('hashchange',()=>{
+    const id = location.hash.slice(1)
+    ajax.open('GET',contentURL.replace('@id',id),false);
+    ajax.send();
+    const newsContent = JSON.parse(ajax.response)
+    root.innerHTML=''
+    const title = document.createElement('h1')
+    title.innerHTML=newsContent.title
+    content.appendChild(title)
+    root.appendChild(content)
+})
+
 for(let i=0;i<10;i++){
     const a = document.createElement('a')
     const li = document.createElement('li')
-    a.setAttribute('href',`${newsFeed.id}`)
+    a.href=`#${newsFeed[i].id}`;
+
     li.appendChild(a)
     a.innerHTML=`${newsFeed[i].title} (${newsFeed[i].comments_count})`
     ul.appendChild(li)

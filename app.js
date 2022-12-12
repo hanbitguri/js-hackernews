b969088ebc104f6aa639222b0db14adf0a6aa5dc
전한빛 <asjask123@gmail.com>
2022-12-12 14:35:11 +0900

    remove dom api codes

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 const ajax = new XMLHttpRequest();
 const newsURL = 'https://api.hnpwa.com/v0/news/1.json'
 const contentURL = `https://api.hnpwa.com/v0/item/@id.json`
-const content = document.createElement('div')
 
 function network(url){
     ajax.open('GET',url,false);
     ajax.send();
     return JSON.parse(ajax.response)
 }
-const newsFeed = network(newsURL)
 
+const newsFeed = network(newsURL)
 const root = document.querySelector('.root')
-const ul = document.createElement('ul')
+const content = document.createElement('div')
 
 window.addEventListener('hashchange',()=>{
     const id = location.hash.slice(1)
     const newsContent = network(contentURL.replace('@id',id))
     
-    root.innerHTML=''
-    const title = document.createElement('h1')
-    title.innerHTML=newsContent.title
-    content.appendChild(title)
-    root.appendChild(content)
+
+    root.innerHTML=`
+    <h1>${newsContent.title}</h1>
+    <div>
+    <a href="/">목록으로</a>
+    </div>
+    `;
+
+
+    
+    
 })
 
+const newsList = []
+newsList.push('<ul>')
 for(let i=0;i<10;i++){
-    const a = document.createElement('a')
-    const li = document.createElement('li')
-    a.href=`#${newsFeed[i].id}`;
-
-    li.appendChild(a)
-    a.innerHTML=`${newsFeed[i].title} (${newsFeed[i].comments_count})`
-    ul.appendChild(li)
-
+    newsList.push(
+    `
+    <li>
+    <a href="#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+    `)
 }
+newsList.push('</ul>')
 
-root.appendChild(ul)
+
+root.innerHTML = newsList.join('')
